25ff0b349b9e553864550ef0d0d2cb2b8480ae8c
Elliott Kember <elliott.kember@gmail.com>
2024-09-30 11:07:00 +1300

    REmove output

--- test.ts
+++ test.ts
@@ -24,16 +24,25 @@ on(Actions.main.initialize, Actions.main.wakeUp).do(
   },
 );
 
-// Expected output:
+// Desired output:
+// prettier-ignore
 
-on(Actions.auth.refreshLogin).do(
+on(
+  Actions.auth.refreshLogin
+).do(
   "Clear cookies on auth credentials reset",
   ({ done }) => {
     done();
   },
 );
 
-on(Actions.main.initialize, Actions.main.wakeUp).do(
+// Desired output:
+// prettier-ignore
+
+on(
+  Actions.main.initialize,
+  Actions.main.wakeUp
+).do(
   "Check if the user needs to be reauthenticated",
   ({ done }) => {
     done();
